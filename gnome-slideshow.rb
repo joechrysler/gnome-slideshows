50e2155f780e66b138238ecef2dcc7c5e03ace6a
Joe Chysler <mail@joechrysler.com>
2011-05-31 16:53:37 -0400

    Use these brand new technologiez: arguments

--- gnome-slideshow.rb
+++ gnome-slideshow.rb
@@ -1,5 +1,6 @@
-# Iterates through the current directory looking for pngs and jpgs
-# globs them into an xml file that gnome-desktop can understand
+#!/usr/bin/env ruby
+# Iterates through a given directory looking for given file types,
+# globs them into an xml file that gnome-desktop can understand.
 
 puts "<background>"
 puts "  <starttime>"
@@ -11,39 +12,40 @@ puts "    <minute>00</minute>"
 puts "    <second>00</second>"
 puts "  </starttime>"
 
-previous = ''
-image_duration = 900.0
-transition_duration = 5.0
-path = `pwd`
-num = 0
-firstRun = true
-first = ''
+files = Array.new
+@image_duration = 900.0
+@transition_duration = 5.0
 
-Dir.glob("./*.*p*g") do |image|
-  image.slice!(0)
-  image = "#{path.chomp}#{image.chomp}"
-  first = image if firstRun
-  unless previous == ''
-    puts "  <transition>"
-    puts "    <duration>#{transition_duration}</duration>"
-    puts "    <from>#{previous}</from>"
-    puts "    <to>#{image}</to>"
-    puts "  </transition>"
+# Process Arguments
+path = ARGV[0] unless ARGV.empty? 
+
+ARGV[1..-1].each do |argument|
+  Dir["#{path}*.#{argument}"].each do |image|
+    files << image
   end
-  previous = image
-
-  puts "  <static>"
-  puts "    <duration>#{image_duration}</duration>"
-  puts "    <file>#{image}</file>"
-  puts "  </static>"
-
-  firstRun = false
 end
 
-puts "  <transition>"
-puts "    <duration>#{transition_duration}</duration>"
-puts "    <from>#{previous}</from>"
-puts "    <to>#{first}</to>"
-puts "  </transition>"
+def put_image(file)
+  puts "  <static>"
+  puts "    <duration>#{@image_duration}</duration>"
+  puts "    <file>#{file}</file>"
+  puts "  </static>"
+end
+
+def put_transition(current_file, next_file)
+  puts "  <transition>"
+  puts "    <duration>#{@transition_duration}</duration>"
+  puts "    <from>#{current_file}</from>"
+  puts "    <to>#{next_file}</to>"
+  puts "  </transition>"
+end
+
+files.each_index do |index|
+  put_image(files[index])
+  put_transition(files[index], files[index+1]) unless index == files.length
+  if index == files.length - 1
+    put_transition(files[index], files[0])
+  end
+end
 
 puts "</background>"
